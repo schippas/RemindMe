6876a3278e50fed084a629bf02375fd8424f8c22
aabrown1103 <43011736+aabrown1103@users.noreply.github.com>
2019-04-21 01:30:26 -0400

    Update logoBanner.jsx

--- client/src/components/logoBanner.jsx
+++ client/src/components/logoBanner.jsx
@@ -4,14 +4,12 @@ import SignUp from "./signup";
 import "./logoBanner.css";
 
 class LogoBanner extends Component {
-<<<<<<< HEAD
 
 onSignUp(){
   
 }
 
-=======
->>>>>>> 34523e1866939ce667de13a95332847f7e7fa522
+  
   render() {
     return (
       <div className="logo">
