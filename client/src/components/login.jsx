6be59d8212bb8d52dc9bfe164de313edde3795d2
Aaron C Brown <aaronbrwn555@gmail.com>
2019-04-22 02:10:41 -0400

    create event

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -65,13 +65,8 @@ class Login extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <h1 className = "login_title">Log In </h1>
-  
-=======
         <h1 className="login_title">Log In </h1>
 
->>>>>>> 7264da2b13620d1bd8bf52a30f9f63b5ab786c5b
         <form>
           <fieldset>
             <p>
@@ -87,16 +82,10 @@ class Login extends Component {
         </form>
 
         <div>
-<<<<<<< HEAD
-      
-          <button className= "submit_button" id="login_button" onClick={this.onClickLogin}> Login </button>
-          <button className= "submit_button" id="forgot_button" onClick={this.onClickForgot}>Forgot password</button>
-
-=======
           <button
             className="submit_button"
             id="login_button"
-            onClick={this.handleLogIn}
+            onClick={this.onClickLogin}
           >
             {" "}
             Login{" "}
@@ -104,11 +93,10 @@ class Login extends Component {
           <button
             className="submit_button"
             id="forgot_button"
-            onClick={this.handleLogIn}
+            onClick={this.onClickForgot}
           >
             Forgot password
           </button>
->>>>>>> 7264da2b13620d1bd8bf52a30f9f63b5ab786c5b
         </div>
       </div>
     );
