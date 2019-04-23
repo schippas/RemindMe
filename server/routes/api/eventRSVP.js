81de332a84a8c373064fa9a6f371f20f65f18ae1
schippas <schippas@purdue.edu>
2019-04-23 06:06:54 -0400

    Started adding listEvents api call

--- server/routes/api/eventRSVP.js
+++ server/routes/api/eventRSVP.js
@@ -19,8 +19,18 @@ app.post('/api/RSVP', function(req,res) {
 	console.log(event_id)
 	
 	//send sql query to update event attendees
-	sql.query( 'INSERT INTO attendees (event_id, user_id) VALUES (?, ?)', [event_id, user_id]);
+	sql.query( 'INSERT INTO attendees (event_id, user_id) VALUES (?, ?)', [event_id, user_id],	
+		function( err, rsql ) {
+               		if ( err ){
+                        	console.log( "sql error: ", err );
+                        	es.json({"status":"fail","reason":"sql error"});
+                	}else{
+                    		console.log( "RSVP worked!" )
+                    		res.json({ "status":"okay","event":event_id});
 
+                	}
+            	}
+	);
 
 });
 
@@ -28,8 +38,31 @@ app.post('/api/RSVP', function(req,res) {
 //handles returning events to the frontend.
 app.post('/api/listEvents', function(req,res) {
 	
-	
+	//should create array of events?
+	sql.query('SELECT events.*',
+		function( err, rsql ) {
+               		if ( err ){
+                        	console.log( "sql error in listEvents: ", err );
+                        	es.json({"status":"fail","reason":"sql error"});
+                	}else{
+                    		console.log( "List Working?" )
+                    		//res.json({ "status":"okay","event":event_id});
+				var data = []
+				for(var i = 0; i<rsql.length; i++){
+					data.push({
+						"event_id":rsql[i].event_id,
+						"event_name":rsql[i].event_name,
+						"event_info":rsql[i].event_info,
+						"event_date":rsql[i].event_date,
+						"event_time":rsql[i].event_time
+					})
+				}
+				res.json({ "status":"okay","events":data});
 
+                	}
+            	}
+	);
+	
 
 
 });
