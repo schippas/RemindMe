8b8794a4bfcc7d40c08c65fd3acd2bb2509239cb
Aaron C Brown <aaronbrwn555@gmail.com>
2019-04-23 01:21:00 -0400

    added calendar library

--- client/src/MainEventPage.js
+++ client/src/MainEventPage.js
@@ -1,47 +1,54 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
+//import Calendar from "react-calendar";
 import "./mainEventPage.css";
+//                      <Calendar onChange={this.onChange} value={this.state.date} calendarType="US"></Calendar>
 
 class MainEventPage extends Component {
-  handleClickEvent() {
-    //this.setState({ createNewEvent: true });
-    console.log("create event redirect");
-    window.location.replace("/create");
-  }
+    handleClickEvent() {
+        //this.setState({ createNewEvent: true });
+        console.log("create event redirect");
+        window.location.replace("/create");
+    }
 
-  onMainEventClick() {
-    window.location.replace("/events");
-  }
+    onMainEventClick() {
+        window.location.replace("/events");
+    }
 
-  onLoginClick() {
-    window.location.replace("/");
-  }
+    onLoginClick() {
+        window.location.replace("/");
+    }
 
-  state = {
-    createNewEvent: false,
-    events: [
-      //fetch events here
-    ]
-  };
+    state = {
+        date: new Date(),
+        createNewEvent: false,
+        events: [
+            //fetch events here
+        ]
+    };
 
-  render() {
-    return (
-      <div className="logo">
-        <div className={"bannerStyle"}>
-          <h1>RemindMe</h1>
-        </div>
-        <div>
-          <button onClick={this.onMainEventClick}>Main Event Page</button>
-          <button onClick={this.handleClickEvent}>Create Event Now</button>
-          <button onClick={this.onLoginClick}>Login/Signup</button>
-        </div>
-        <div className={"box"}>
-          <div className={"calendar"}>Calendar Here</div>
-          <div className={"eventList"}>Event List Here</div>
-        </div>
-      </div>
-    );
-  }
+    onChange = date => this.setState({ date })
+
+    render() {
+        return (
+            <div className="logo">
+                <div className={"bannerStyle"}>
+                    <h1>RemindMe</h1>
+                </div>
+                <div>
+                    <button onClick={this.onMainEventClick}>Main Event Page</button>
+                    <button onClick={this.handleClickEvent}>Create Event Now</button>
+                    <button onClick={this.onLoginClick}>Login/Signup</button>
+                </div>
+                <div className={"box"}>
+                    <div className={"calendar"}>Calendar Here
+
+                    </div>
+                    <div className={"eventList"}>Event List Here</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withRouter(MainEventPage);
