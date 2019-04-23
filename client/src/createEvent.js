3d6f0ccbebe761ad513a57c3bd13e22b082c52a1
Aaron C Brown <aaronbrwn555@gmail.com>
2019-04-22 21:25:14 -0400

    blah

--- client/src/createEvent.js
+++ client/src/createEvent.js
@@ -8,27 +8,33 @@ class CreateEvent extends Component {
     window.location.replace("/events");
   }
 
-handleSubmit() {
-	console.log("submitting event");
-	var event_name = document.getElementById("eventName").value;
-	var event_info = document.getElementById("eventDetails").value;
-	var event_date = document.getElementById("eventDate").value;
-	var event_time = document.getElementById("eventTime").value;
-	var user_id = localStorage.account;
+  handleSubmit() {
+    console.log("submitting event");
+    var event_name = document.getElementById("eventName").value;
+    var event_info = document.getElementById("eventDetails").value;
+    var event_date = document.getElementById("eventDate").value;
+    var event_time = document.getElementById("eventTime").value;
+    var user_id = localStorage.account;
 
-     	document.getElementById('eventName').value = "";
-     	document.getElementById('eventDetails').value = "";
-     	document.getElementById('eventDate').value = "";
-     	document.getElementById('eventTime').value = "";
+    document.getElementById("eventName").value = "";
+    document.getElementById("eventDetails").value = "";
+    document.getElementById("eventDate").value = "";
+    document.getElementById("eventTime").value = "";
     //window.location.replace("/events");
-	var data = JSON.stringify({"event_name":event_name, "event_info":event_info, "event_date":event_date, "event_time":event_time, "user_id":user_id});
+    var data = JSON.stringify({
+      event_name: event_name,
+      event_info: event_info,
+      event_date: event_date,
+      event_time: event_time,
+      user_id: user_id
+    });
 
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "/api/createEvent", true);
-     	xhttp.setRequestHeader("Content-Type", "application/json");
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/api/createEvent", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
 
-xhttp.send(data);
-};
+    xhttp.send(data);
+  }
 
   loginReturn() {
     window.location.replace("/");
@@ -36,16 +42,6 @@ xhttp.send(data);
 
   render() {
     return (
-<<<<<<< HEAD
-      <div className={"logo"}>
-        <div className={"bannerStyle"}>
-          <h1 className={"float-left-child"}>RemindMe</h1>
-        </div>
-        <div className={"float-right-child"}>
-          <button>Create Event</button>
-          <button onClick={this.handleReturn}>
-            Return to Main Events Page
-=======
       <div className="pg_bgd">
         <h1 className="headerBar"> RemindMe </h1>
         <div className="options_bar">
@@ -68,7 +64,6 @@ xhttp.send(data);
           >
             {" "}
             Login/Signup{" "}
->>>>>>> 4234ddc6bff34f92aa4782a095fe14cc98fe5d46
           </button>
         </div>
         <div className="box">
