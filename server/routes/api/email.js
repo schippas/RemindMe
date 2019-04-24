69aef4bf62adff420d8fb366348aa364005dc52d
schippas <schippas@purdue.edu>
2019-04-24 02:13:55 -0400

    fixed now hopefully

--- server/routes/api/email.js
+++ server/routes/api/email.js
@@ -13,20 +13,20 @@ app.post('/api/email', function(req, res){
 	
 	//sql query to find email address, and send email.
 	sql.query( 'SELECT email, username FROM accounts WHERE id=?;', [user_id],
-		functon(err, rsql){
-			if(err){
-				console.log("email failed", err);
-				res.json({"status":"fail", "reason":"sql error"})
-			}else{
+		//functon(err, rsql){
+		//	if(err){
+		//		console.log("email failed", err);
+		//		res.json({"status":"fail", "reason":"sql error"});
+		//	}else{
 				
-			}
+		//	}
 			
 					
-		}
+		//}
 	
 	);
 
 
-});
+})
 
 }
