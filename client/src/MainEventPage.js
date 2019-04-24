ae068dbee1ccf61eea9492888cf6d7a6008580dd
Aaron C Brown <aaronbrwn555@gmail.com>
2019-04-24 02:40:05 -0400

    dynamic display should work

--- client/src/MainEventPage.js
+++ client/src/MainEventPage.js
@@ -6,20 +6,38 @@ import Calendar from "react-calendar";
 
 class MainEventPage extends Component {
 
+
+    /*Event(e_id, e_name, e_info, e_date, e_time) {
+        this.event_id = e_id;
+        this.event_name = e_name;
+        this.event_info = e_info;
+        this.event_date = e_date;
+        this.event_time = e_time;
+        //this.isToggleOn = true;
+
+    }*/
+
+
     constructor(props) {
         super(props);
-        this.state = { isToggleOn: true };
+        this.state = {
+            isToggleOn: true,
+
+            //this.Event('2', 'CS', 'BIDC', 'Wed Apr 24', '1:00')
+
+            //new Event('1', 'Bowling', 'At URR', 'Mon Apr 22', '5:15')
+            events: [{ e_id: '1', e_name: 'Bowling Party', e_info: 'At the Union Rack & Roll', e_date: 'Mon Apr 22', e_time: '1:00 PM' },
+            { e_id: '2', e_name: 'CS', e_info: 'At BIDC', e_date: 'Wed Apr 24', e_time: '2:00 AM' },
+            { e_id: '3', e_name: 'Final', e_info: 'Elliot Hall', e_date: 'Fri Apr 26', e_time: '8:00 AM' }],
+            date: new Date(),
+        };
 
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
     }
 
-    state = {
-        date: new Date(),
-    }
 
     handleClickEvent() {
-        //this.setState({ createNewEvent: true });
         console.log("create event redirect");
         window.location.replace("/create");
     }
@@ -42,12 +60,7 @@ class MainEventPage extends Component {
         this.setState({ date })
     }
 
-    state = {
-        createNewEvent: false,
-        events: [
-            //fetch events here
-        ]
-    };
+
 
     loginReturn() {
         window.location.replace("/");
@@ -97,8 +110,23 @@ class MainEventPage extends Component {
                     <div>
                         <input className="which_week" type="text" id="weekBox" value={this.state.date} />
                     </div>
-
-                    <div className="each-box">
+                    <div>
+                        <ul>
+                            {
+                                this.state.events.map((event) => (
+                                    <li key={event}>
+                                        <div className="each-box">
+                                            <button className="togglebtn" onClick={this.handleClick}>
+                                                {this.state.isToggleOn ? 'Interested' : 'Not Interested'}
+                                            </button>
+                                            <h1 className="inBoxEventDescription">{event.e_name + ': ' + event.e_info + ' ' + event.e_date + ' at ' + event.e_time}</h1>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    {/*<div className="each-box">
 
                         <button className="togglebtn" onClick={this.handleClick}>
                             {this.state.isToggleOn ? 'Interested' : 'Not Interested'}
@@ -106,7 +134,10 @@ class MainEventPage extends Component {
 
                         <h1 className="inBoxEventDescription">fasfasd</h1>
 
-                    </div>
+
+
+        </div>*/}
+
 
                     {/* <div className="each-box2">
                   
